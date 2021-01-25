031f2b18f507fdb86b24bf38aba035a371f330d0
Islam Hassan <islamhassan4444@gmail.com>
2021-01-25 11:52:41 +0200

    My-Shopping

---
+++ src/static/places.js
@@ -0,0 +1,19 @@
+
+const places = [
+    {
+      title: 'Sale shop',
+      description:
+        "Sale! Yay! Tons of fab Monki pieces just became real steals. Are you ready to bring those bargains home? Go get 'em gurl Find all new arrivals in new in.For the latest .",
+      imageUrl: process.env.PUBLIC_URL + '/assets/sale.jpg',
+      time: 1500,
+    },
+    {
+      title: 'Gift Cards',
+      description:
+        'Buy an e-gift card online and get it sent straight to your inbox or your recipient’s inbox. You can schedule your own delivery date and time or opt to send straight.',
+      imageUrl: process.env.PUBLIC_URL + '/assets/member card.jpg',
+      time: 1500,
+    },
+  ];
+  
+  export default places;
