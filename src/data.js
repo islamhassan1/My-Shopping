c809b1ddf7acc7878fc94e5bb7be6094257b4008
Islam Hassan <islamhassan4444@gmail.com>
2021-01-26 18:39:33 +0200

    added data

---
+++ src/data.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  FaBehance,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaSketch,
+  FaHome,
+  FaAngellist,
+  FaShopify,
+  FaReadme,
+  FaTwitch,
+  FaSistrix,
+  FaCcAmazonPay,
+  FaViber,
+} from 'react-icons/fa';
+export const links = [
+  {
+    id: 1,
+    url: '/home',
+    text: 'home',
+    icon: <FaHome />,
+  },
+  {
+    id: 2,
+    url: '/New In',
+    text: 'New In',
+    icon: <FaAngellist />,
+  },
+  {
+    id: 3,
+    url: '/Find A Products',
+    text: 'Find A Products',
+    icon: <FaSistrix />,
+  },
+  {
+    id: 4,
+    url: '/Categories',
+    text: 'Categories',
+    icon: <FaShopify />,
+  },
+  {
+    id: 5,
+    url: '/Our Reviews',
+    text: 'Our Reviews',
+    icon: <FaReadme />,
+  },
+  {
+    id: 6,
+    url: '/Intro',
+    text: 'Intro',
+    icon: <FaTwitch />,
+  },
+  {
+    id: 7,
+    url: '/Membership card',
+    text: 'Membership card',
+    icon: <FaCcAmazonPay />,
+  },
+  {
+    id: 8,
+    url: '/Contact Us',
+    text: 'Contact Us ',
+    icon: <FaViber />,
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.facebook.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://www.behance.com',
+    icon: <FaBehance />,
+  },
+  {
+    id: 5,
+    url: 'https://www.sketch.com',
+    icon: <FaSketch />,
+  },
+];
