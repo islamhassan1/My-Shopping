308a4d3502569235325c046a633f94f21a077dc7
Islam Hassan <islamhassan4444@gmail.com>
2021-01-27 17:30:48 +0200

    added Categories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
 import PlaceToVisit from './components/PlaceToVisit';
 import Sidbar from './components/Sidebar';
 import Modal from './components/Modal';
+import Categories from './components/Categories';
+import Menu from './components/Menu';
+import items from './data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minHeight: '100vh',
+    height: '1500PX',
     backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
@@ -16,6 +20,17 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function App() {
   const classes = useStyles();
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -23,6 +38,16 @@ export default function App() {
       <Header />
       <Modal />
       <PlaceToVisit />
+       <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Categories</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
       
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+/*Sidebar*/
 import React from 'react';
 import {
   FaBehance,
@@ -92,3 +93,85 @@ export const social = [
     icon: <FaSketch />,
   },
 ];
+
+
+
+/* MENU*/
+
+const menu = [
+  {
+    id: 1,
+    title: 'Men suit',
+    category: 'Men',
+    price: 15.99,
+    img: '/src/components/img/item-1.jpg',
+    desc: `Charcoal grey solid formal suit
+    Charcoal grey solid super slim-fit formal blazer, has a notched lapel, single-breasted with double button closures `,
+  },
+  {
+    id: 2,
+    title: 'Women Green Top',
+    category: 'Woman',
+    price: 13.99,
+    img: '/src/components/img/item-4.jpg',
+    desc: `Green embroidered regular top and has a round neck, three quarter sleeves . `,
+  },
+  {
+    id: 3,
+    title: 'Boys T-shirts',
+    category: 'Kids',
+    price: 6.99,
+    img: '/src/components/img/item-7.jpg',
+    desc: `Pack of five knitted T-shirts in red, green, blue, green, white and black, has a contrast ribbed round neck, short sleeves with contrast ribbed cuffs, graphic print on the front, straight hem.`,
+  },
+  {
+    id: 4,
+    title: 'Men sweatshirt',
+    category: 'Men',
+    price: 20.99,
+    img: '/src/components/img/item-2.jpg',
+    desc: `Mustard yellow printed pullover sweatshirt, has a hood, two pockets, long sleeves, straight hem . `,
+  },
+  {
+    id: 5,
+    title: 'Women Jacket',
+    category: 'Woman',
+    price: 22.99,
+    img: '/src/components/img/item-5.jpg',
+    desc: `Maroon solid quilted jacket, has a stand collar, 2 pockets, zip closure, long sleeves, straight hem, and polyester lining .`,
+  },
+  {
+    id: 6,
+    title: 'Girls Dress',
+    category: 'Kids',
+    price: 18.99,
+    img: '/src/components/img/item-8.jpg',
+    desc: `Peach-Coloured printed woven fit and flare dress, has a round neck, short sleeves, concealed zip closure, and flared hem .`,
+  },
+  {
+    id: 7,
+    title: 'Men jeans jacket',
+    category: 'Men',
+    price: 8.99,
+    img: '/src/components/img/item-3.jpg',
+    desc: `Blue solid jacket, has a stand collar, 4 pockets, zip closure, long sleeves, straight hem, and unlined .`,
+  },
+  {
+    id: 8,
+    title: 'Women T-shirt',
+    category: 'Woman',
+    price: 12.99,
+    img: '/src/components/img/item-6.jpg',
+    desc: `Yellow and Beige printed T-shirt, has a round neck, and short sleeves .  `,
+  },
+  {
+    id: 9,
+    title: 'Kids Jacket',
+    category: 'Kids',
+    price: 16.99,
+    img: '/src/components/img/item-10.jpg',
+    desc: `Purple and Red printed jacket, has a hooded, 2 pockets, zip closure, long sleeves, straight hem, and polyester lining .`,
+  },
+];
+
+export default menu;
